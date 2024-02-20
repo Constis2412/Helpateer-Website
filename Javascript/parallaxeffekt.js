3815fe4134b7dc9dec4a22f7e9dbdf5f0fded755
andreea <coj22628@spengergasse.at>
2024-02-20 21:02:32 +0100

    parallaxe effect succsefully implemented, addionally the menubar created

---
+++ Javascript/parallaxeffekt.js
@@ -0,0 +1,8 @@
+let text = document.getElementById('p_text')
+let people = document.getElementById('people')
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    text.style.marginTop = value*1.5 + 'px';
+    people.style.marginTop = value*0.5 + 'px';
+})
