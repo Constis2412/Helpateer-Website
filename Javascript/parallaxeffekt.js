a3d8268f7db96a2757b4ea6cc788c189f168aceb
Constis2412 <pfe20424@spengergasse.at>
2024-02-21 11:50:06 +0100

    forgot.js

--- Javascript/parallaxeffekt.js
+++ Javascript/parallaxeffekt.js
@@ -3,11 +3,7 @@ let people = document.getElementById('people')
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-<<<<<<< HEAD
     text.style.marginTop = value*2 + 'px';
     people.style.marginTop = value*0.5 + 'px';
-=======
-    text.style.marginTop = value*1.5 + 'px';
-    people.style.marginTop = value*-0.5 + 'px';
->>>>>>> 6197ccb5ce961d8954506e5b991d97122fca6ff8
+
 })
