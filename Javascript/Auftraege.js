abc0bd2130fc6f04ca0fc793b24b04cec76aadca
Jasmin-ElIbrahim <el190279@spengergasse.at>
2024-02-22 20:40:31 +0100

    Navigation inkl. Slide done
    JS Verlinkung hinzugefügt,  Slide und toggle Verschiebung abgeschlossen

--- Javascript/Auftraege.js
+++ Javascript/Auftraege.js
@@ -1,3 +1,4 @@
+//adding hovered class
 let list = document.querySelectorAll(".nav li");
 
 function activeLink(){
@@ -7,4 +8,14 @@ function activeLink(){
     this.classList.add("hovered");
 }
 
-list.forEach(item => item.addEventListener("mouseover", activeLink))
+list.forEach((item) => item.addEventListener("mouseover", activeLink))
+
+//Menu Toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".nav")
+let main = document.querySelector(".main");
+
+toggle.onclick = function(){
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+}
